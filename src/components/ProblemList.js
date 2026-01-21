ceebd20762900db91ec65f2c1a6f0fdd8c491a6b
hieu-van-nguyen <hieuacctforgcp@gmail.com>
2026-01-20 23:50:24 -0600

    sort problems by category

--- src/components/ProblemList.js
+++ src/components/ProblemList.js
@@ -5,10 +5,13 @@ import { problemsColRef } from "../firebase/config";
 import { addDoc, getDocs, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 
 function ProblemList() {
-  const [data, setData] = useState([]);  
+  const [data, setData] = useState([]); 
+  const [solved, setSolved] = useState(0);
+
   const columns = [
     { field: "category", headerName: "Category", width: 90 },
     { field: "problem", headerName: "Problem", width: 150, editable: true },
+    { field: "difficulty", headerName: "Difficulty", width: 150, editable: true },
     {
       field: "javaSolution",
       headerName: "Java Solution",
@@ -29,8 +32,18 @@ function ProblemList() {
       id: doc.id,
       category: doc.data().category,
       problem: doc.data().problem,
+      difficulty: doc.data().difficulty,
       javaSolution: doc.data().javaSolution
     }));
+    problemList.sort((a, b) => {
+      if (a.category < b.category) {
+        return -1;
+      } else if (a.category > b.category) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
     setData(problemList);
   }
 
@@ -41,7 +54,10 @@ function ProblemList() {
   return (
     <Container style={{ marginTop: 40 }}>
       <Typography variant="h5" gutterBottom>
-        Problem List
+        Problem List: {data.length}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Problem Solved: {solved}
       </Typography>
 
       <div style={{ height: 400, width: "100%" }}>
